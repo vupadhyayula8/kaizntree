4587268d7be183b51a128d857a0edddd378fc19a
vinayupadhyayula <vupadhyayula@binghamton.edu>
2024-02-11 13:34:48 -0500

    django setup and login api

--- kaizntree_back_end/kaizntree/views.py
+++ kaizntree_back_end/kaizntree/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def login(request):
+    if request.method == "POST":
+        print(request.POST.get)
+        print(request.POST)
+        print("came here")
+    return HttpResponse("Login")
+
+
+def register(request):
+    return HttpResponse("Register")
+
+
+def fetch_items(request):
+    return HttpResponse("Fetch items")
+
+
+def add_item(request):
+    return HttpResponse("Add item")
+
+
+def add_category(request):
+    return HttpResponse("Add category")
+
--- kaizntree_back_end/kaizntree_back_end/settings.py
+++ kaizntree_back_end/kaizntree_back_end/settings.py
@@ -37,13 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'corsheaders',
+    'kaizntree'
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -121,3 +124,28 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:4200',
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS = [
+    "127.0.0.1", 
+    "localhost",
+    # "*"
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1", 
+    "http://localhost:4200", 
+]
+# CORS_ALLOW_CREDENTIALS = False
+
+# CSRF_TRUSTED_ORIGINS = ["http://localhost:4200"]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST"
+]
+
